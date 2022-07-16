fc90cb0b6a4216d05ab757660c1bfde50f8be409
After拂晓 <1810732817@qq.com>
2022-07-16 13:32:37 +0800

    项目初始化结构

---
+++ mall-order/src/test/java/com/wangc/anothermall/order/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangc.anothermall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
